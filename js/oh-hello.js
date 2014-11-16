9020e5137861252886e6ad6ae6afb03059572f7c
sshifflett <Stephanie@Stephanies-iMac.local>
2014-11-16 18:51:07 -0500

    update
    update

--- js/oh-hello.js
+++ js/oh-hello.js
@@ -1,1 +1,15 @@
-$('body').scrollspy({target: '#bs-example-navbar-collapse-1'})
+$(document).ready(function(){
+	$('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up"></span> Back to Top</div>');
+		$(window).scroll(function () {
+			if ($(this).scrollTop() != 0) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		}); 
+
+		$('#toTop').click(function(){
+			$("html, body").animate({ scrollTop: 0 }, 600);
+			return false;
+		});
+});
